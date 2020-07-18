87da7abcdc55f3da7a9bb0aaf11bd454560d3e8b
Anton Starichenkov <anton.starichenkov@gmail.com>
2020-07-18 15:23:01 +0300

    2. Сокобан. Тестовое задание.[75283#224291]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,20 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
+        boolean rsl = false;
+        StringBuilder row = new StringBuilder();
+        StringBuilder column = new StringBuilder();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                row.append(board[j][i]);
+                column.append(board[i][j]);
+            }
+            row.append("-");
+            column.append("-");
+        }
+        if (row.toString().contains("11111") || column.toString().contains("11111")) {
+            rsl = true;
+        }
         return rsl;
     }
 }
